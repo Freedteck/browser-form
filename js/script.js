e508cbeef74d4cfbe7873a4ed19c341f2591ee8c
Freedteck <freedteck@gmail.com>
2023-11-30 12:34:10 +0100

    more validation added

--- js/script.js
+++ js/script.js
@@ -5,38 +5,51 @@ const zip = document.getElementById('zip')
 const password = document.getElementById('password')
 const confPassword = document.getElementById('confirm')
 const span = document.querySelectorAll('span')
+const submit = document.getElementById('submit')
+
+
+const clearSpans = () => {
+    span.forEach(span => {
+        span.innerHTML = ''
+    })
+}
+
+const checkInputs = (input, index) => {
+    if (input.validity.valid) {
+        span[index].textContent = ''
+    }
+    if (input.name === 'mail') {
+        clearSpans()
+        emailError()
+    }
+    if (input.name === 'password') {
+        clearSpans()
+        passwordError()
+    }
+    if (input.name === 'confirm') {
+        clearSpans()
+        confirmPasswordError()
+    }
+}
 
 input.forEach((inp, index) => {
     inp.addEventListener('input', () => {
-        if (inp.validity.valid) {
-            span[index].textContent = ''
-        } else if (inp.name === 'mail') {
-            emailError()
-        } else if (inp.name === 'password') {
-            passwordError()
-        } else if (inp.name === 'confirm') {
-            confirmPasswordError()
-        }
+        checkInputs(inp, index)
     })
 })
-// email.addEventListener('input', () => {
-//     if (email.validity.valid) {
-//         span[0].textContent = ''
-//     }
-//     showError()
-// })
-// password.addEventListener('input', () => {
-//     if (password.validity.valid) {
-//         span[3].textContent = ''
-//     }
-//     showError()
-// })
-// confPassword.addEventListener('input', () => {
-//     if (confPassword.validity.valid) {
-//         span[4].textContent = ''
-//     }
-//     showError()
-// })
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (!email.validity.valid) {
+        emailError()
+    } else if (!password.validity.valid) {
+        passwordError()
+    } else if (!confPassword.validity.valid) {
+        confirmPasswordError()
+    } else {
+        console.log('Congratulations');
+    }
+})
 
 function emailError() {
 
@@ -46,6 +59,7 @@ function emailError() {
         span[0].textContent = 'email address is expected'
     }
 }
+
 function passwordError() {
 
     if (password.validity.valueMissing) {
@@ -54,9 +68,9 @@ function passwordError() {
         span[2].textContent = 'password must be at least 8 characters'
     }
 }
+
 function confirmPasswordError() {
     if (confPassword.value !== password.value) {
         span[3].textContent = 'password not match'
-        console.log(confPassword.value, password.value);
     }
 }
